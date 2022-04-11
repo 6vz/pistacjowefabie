576970fe063ea30a68d9046a6ea62ebbd574780c
Mateusz (6vz) <mateuszbaranowski@protonmail.com>
2022-04-11 17:20:05 +0200

    another fucking aaa

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,17 @@
 function changePage(pageName) {
-    const page = pageName + ".html"
-    if (pageName == "index") {
-        window.location.href = "index.html"
-    } else if (pageName = "galeria") {
-        window.location.href = "galeria.html"
-    } else if (pageName = "danetechniczne") {
-        window.location.href = "danetechniczne.html"
-    } else if (pageName = "kalkulator") {
-        window.location.href = "kalkulator.html"
-    } else if (pageName = "zamow") {
-        window.location.href = "zamow.html"
-    } else if (pageName = "kontakt") {
-        window.location.href = "kontakt.html"
+    const pages = [
+        'index',
+        'galeria',
+        'danetechniczne',
+        'kalkulator',
+        'zamow',
+        'kontakt',
+    ];
+    if (pages.includes(pageName)) {
+        window.location.assign(pageName + '.html');
     } else {
-        alert('Coś się pokićkało... Odśwież stronę, a następnie spróbuj ponownie. Jeśli to nie pomoże, otwórz PR')
+        alert(
+            'Coś się pokićkało... Odśwież stronę, a następnie spróbuj ponownie. Jeśli to nie pomoże, otwórz PR',
+        );
     }
 }
