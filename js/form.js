27e9a0fd59672a00bdc08f87d84b087dc1d32434
Mateusz (6vz) <mateuszbaranowski@protonmail.com>
2022-04-12 13:23:15 +0200

    fherigfuierfdiefoehyf

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 function sendForm() {
     // get input value from name
-    const name = document.getElementById('name').value;
+    // get value of input
+    var name = document.getElementById("name").value;
     alert(`${name}, już cię zlokalizowaliśmy! Spodziewaj się naszego kuriera za 15 minut!`)
 }
