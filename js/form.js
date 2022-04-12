cd20c8adeb034741bbe9785422991568f0ded460
Mateusz (6vz) <mateuszbaranowski@protonmail.com>
2022-04-12 13:19:48 +0200

    fkwejfe\

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 function sendForm() {
     // get input value from name
     const name = document.getElementById('name').value;
-    const surname = document.getElementById('surname').value;
     alert(`${name}, już cię zlokalizowaliśmy! Spodziewaj się naszego kuriera za 15 minut!`)
 }
