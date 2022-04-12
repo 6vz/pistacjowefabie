44910f269a9dbeb65d22ff8a96e795e390185707
Mateusz (6vz) <mateuszbaranowski@protonmail.com>
2022-04-12 11:59:44 +0200

    hahahahahahahahahah

--- js/alg.js
+++ js/alg.js
@@ -2,7 +2,7 @@ console.log('Witaj na mojej epickiej stronie\nNie powinieneś tego widzieć, ale
 // count from 0 to 10 using while loop
 var i = 0;
 while (i <= 10) {
-    const counter = document.getElementById('counter');
-    counter.innerHTML = `${i} `
+    const sus = document.getElementById('counter');
+    sus.innerHTML = `${i} `
     i++;
 }
